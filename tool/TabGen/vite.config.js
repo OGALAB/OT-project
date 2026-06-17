a16cfcfb6f51135f73646b266acf830fd880e047
ogasawara-shota <shota2000.0415@gmail.com>
2026-06-17 13:15:36 +0900

    カレンダーツールReact＋TS開発： vite.configの修正。Tampermonkey用の宣言、Tampermonkeyのメタデータ記述

--- tool/TabGen/vite.config.js
+++ tool/TabGen/vite.config.js
@@ -1,7 +1,21 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import monkey from 'vite-plugin-monkey';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    monkey({
+      entry: 'src/main.tsx',
+      userscript: {
+        name: 'カレンダーHTML生成／転記ツール',
+        namespace: 'my-calendar-tool',
+        version: '1.0.0',
+        match: ['http://*/*', 'https://*/*'], 
+        author: 'Ogasawara',
+        description: 'テキストの入力からカレンダーHTMLを生成してクリップボードにデータを格納します。',
+      },
+    }),
+  ],
 })
